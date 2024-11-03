04a7ac45f78e32646e83574b5d91aee349281bd2
Ps-WareH <P_PVPVE@163.com>
2024-11-03 22:11:14 +0800

    comment

--- mcTree.py
+++ mcTree.py
@@ -3,24 +3,28 @@ class Node:
     def __init__(self,game_state,parent):
         self.state = game_state
         self.parent = parent
-        self.children = {}
+        self.children = {}#{action: child node}
         self.visits = 0
         self.reward = 0
-        self.fullyExpand = False
-
+        self.fullyExpand = False#所有action都已经加入children
+    #运行时间越长，reward越大，没考虑事物
+    #运行时间越长，消耗健康值越高，这个也没考虑
     def getReward(self):
         if(self.endState()):
             return int(self.state['turn'])
         else:
             return 10000
-
+        
     def endState(self):
         if(len(self.getNextActions())==0 or self.state['you']['health']==0):
             return True
         return False
+    #a：action
+    #返回在这个node perform这个action后的下一个node
     def performAction(self,a):
         from copy import deepcopy
         nextState = deepcopy(self.state)
+        #改头的位置
         if(a=='up'):
             nextState['you']['head']['y']+=1
         elif(a=='down'):
@@ -31,27 +35,28 @@ class Node:
             nextState['you']['head']['x']-=1
         else:
             raise Exception(a)
-  
+
         nextState['turn']+=1
-        
+        #检查是否吃掉棋盘上的事物
         if(nextState['you']['head'] in nextState['board']['food']):
             nextState['board']['food'].remove(nextState['you']['head'])
             nextState['you']['health']=100
         else:
             nextState['you']['health']-=1
-            
+        #去掉尾巴
         nextState['you']['body'].pop()
         nextState['you']['body'].insert(0,nextState['you']['head'])
-        
+        #snakes应该是棋盘上所有蛇的合集，把自己的蛇更新一下
         for q in range(len(nextState['board']['snakes'])):
             if(nextState['board']['snakes'][q]['id']==nextState['you']['id']):
                 nextState['board']['snakes'][q]=deepcopy(nextState['you'])
                 break  
         return Node(nextState,self)
             
-        
+    #下一步所有可能的action，无筛选
     def getNextActions(self):
         res = ['up', 'down', 'left', 'right']
+        
         if self.state['board']['width'] - self.state['you']['head']['x'] == 1:  
             res.remove('right')
         if self.state['you']['head']['x']== 0:                         
@@ -86,6 +91,7 @@ class mcTree:
     def __init__(self,maxTime, maxIter):
         self.maxTime = maxTime
         self.maxIter = maxIter
+    #每次移动产生一个root，从这个root开始search，返回的action就是下一步move
     def search(self,start):
         import time
         self.root = Node(start,None)
@@ -97,18 +103,23 @@ class mcTree:
             node = self.select(self.root)
             t = node
             c = 0
+            #t是我们目前选出的下一个node，在t随机进行一个action，直到endState或者循环>10停止搜索（dfs）
             while(t.endState()==False and c<10):
                 c+=1
-                t = node.performAction(random.choice(t.getNextActions()))
+                t = t.performAction(random.choice(t.getNextActions()))
+            #t在搜索的最后一个节点，算reward，把通往t的路径上每一个node的reward更新（backProp）
             reward = t.getReward()
-            self.backProp(node,reward)
-            
+            self.backProp(t,reward)
+        #从root的child node里面挑出一个reward最高的，返回对应的action，这个是最终结果
         bestC = self.getBestChild(self.root, 0)
-        action = (action for action, node in self.root.children.items()
-                  if node is bestC).__next__()
-        
+        action = None
+        for nextA in self.root.children.keys():
+            if(self.root.children[nextA]==bestC):
+                action=nextA
         return action
-            
+    
+    #如果所有children都已经被展开过了，挑reward值最大的那个路径的尽头的node返回，我们接着这个node继续dfs，更新reward
+    #如果没有，随便一个action，下一个node展开（就是dfs）
     def select(self, node):
         while(node.endState()==False):
             if(node.fullyExpand):
@@ -116,16 +127,15 @@ class mcTree:
             else:
                 return self.expand(node)#next node
         return node
-            
+    #这里node应该是dfs搜索的尽头，从root到node路径上的每个节点reward更新
     def backProp(self,node,reward):
         while(node):
             node.visits+=1
             node.reward+=reward
             node= node.parent   
         
-             
+    #选择一个reward比较高的node，v是threshold（lec slides上的公式），大于v的随机返回，如果没有大于v的返回最大的
     def getBestChild(self,node,exploration=0):
-        
         cons = 1/math.sqrt(2)
         bn = list()
         b = float("-inf")
@@ -149,9 +159,7 @@ class mcTree:
         position = bn.index(choice)  # 获取元素在列表中的索引
         print(f"选中的元素位于列表的第 {position + 1} 位。一共{len(bn)}个元素可以选")
         return choice
-        
-
-        
+    #对于node，找到第一个没有被dfs探索过的node
     def expand(self,node):
         actions = node.getNextActions()
         for a in actions:
