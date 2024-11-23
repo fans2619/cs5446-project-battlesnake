3e4d99c326bee1ab5984f3fe13a19a6d8d2fcef4
fans2619 <fans2619@gmail.com>
2024-11-23 09:05:09 +0800

    Deploy MCTS

--- mcts_main.py
+++ mcts_main.py
@@ -51,4 +51,4 @@ def move(game_state: typing.Dict) -> typing.Dict:
 if __name__ == "__main__":
     from server import run_server
 
-    run_server({"info": info, "start": start, "move": move, "end": end}, custom_port=81)
+    run_server({"info": info, "start": start, "move": move, "end": end}, custom_port=80)
